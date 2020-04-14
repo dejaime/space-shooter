b101561d6a21d7b7a271a47a924047108689072f
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-14 20:32:25 -0300

    adding stats to SpaceState

--- src/state/space_state.rs
+++ src/state/space_state.rs
@@ -1,6 +1,13 @@
-use amethyst::prelude::*;
+use amethyst::{core::timing::Time, prelude::*};
 
-pub struct SpaceState;
+pub struct SpaceState {
+    score: u64,
+    distance_travelled: u64,
+    projectiles_fired: u64,
+    damage_dealt: u64,
+    kills: u64,
+
+}
 
 impl SimpleState for SpaceState {
     fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {}
