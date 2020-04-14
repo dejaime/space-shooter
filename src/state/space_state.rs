90965f352cd64c7cf29f835e5b75eef197d01c0e
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-14 20:32:49 -0300

    Initializing stats tracker variables and adding a timer

--- src/state/space_state.rs
+++ src/state/space_state.rs
@@ -7,8 +7,32 @@ pub struct SpaceState {
     damage_dealt: u64,
     kills: u64,
 
+    open_menu_timer: Option<f32>,
 }
 
+
 impl SimpleState for SpaceState {
-    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {}
+    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {
+
+        self.score = 0;
+        self.distance_travelled = 0;
+        self.projectiles_fired = 0;
+        self.damage_dealt = 0;
+        self.kills = 0;
+
+        self.open_menu_timer.replace(1.0);
+    }
+
+    fn update(&mut self, data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        if let Some(mut timer) = self.open_menu_timer.take() {
+            timer -= data.world.fetch::<Time>().delta_seconds();
+            if timer <= 0.0 {
+                // Do it
+            } else {
+                // Timer is not expired yet, putting it back
+                self.open_menu_timer.replace(timer);
+            }
+        }
+        Trans::None
+    }
 }
