850044cda5441d695010ad9c8eaf29db52be1e5a
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-15 16:31:27 -0300

    Adding a central function to load all game sprites as resources

---
+++ src/assets.rs
@@ -0,0 +1,13 @@
+use amethyst::{
+    assets::{AssetStorage, Handle, Loader},
+    core::{timing::Time, transform::Transform},
+    ecs::prelude::{Component, DenseVecStorage, Entity},
+    prelude::*,
+    renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
+    ui::{Anchor, TtfFormat, UiText, UiTransform},
+};
+
+
+fn load_sprite_sheets(world: &mut World) {
+
+}
