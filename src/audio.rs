06e256611e38c88f29a968f9552a19076f24bfbb
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-15 20:47:45 -0300

    Fixing error message trying to read ATTRIBUTION.md as music

--- src/audio.rs
+++ src/audio.rs
@@ -1,17 +1,15 @@
 use amethyst::{
-    assets::{Loader, /*AssetStorage*/},
-    audio::{/*output::Output, Source,*/ OggFormat, SourceHandle, /*AudioSink*/},
+    assets::Loader,
+    audio::{/*output::Output, Source,*/ OggFormat, SourceHandle /*AudioSink*/},
     ecs::{World, WorldExt},
 };
 
-use std::{fs, iter::Cycle, vec::IntoIter};
 use rand::seq::SliceRandom;
+use std::{fs, iter::Cycle, vec::IntoIter};
 
 pub struct Music {
     pub music: Cycle<IntoIter<SourceHandle>>,
 }
-
-
 
 fn load_audio_track(loader: &Loader, world: &World, file: &str) -> SourceHandle {
     loader.load(file, OggFormat, (), &world.read_resource())
@@ -24,7 +22,6 @@ pub fn initialise_music(world: &mut World) {
         let mut tracks = Vec::new();
 
         let audio_files = fs::read_dir("./assets/audio/music").unwrap();
-        
         for file in audio_files {
             let track_file_path = format!("{}", file.unwrap().path().display());
             let split_vec: Vec<&str> = track_file_path.split("./assets/").collect();
@@ -32,7 +29,9 @@ pub fn initialise_music(world: &mut World) {
             if fixed_name.ends_with(".ogg") {
                 tracks.push(format!("{}", fixed_name));
             } else {
-                println! ("[ERROR] Invalid audio extension on file {}, only OGG is supported. Ignoring file.", fixed_name);
+                if !fixed_name.ends_with("ATTRIBUTION.md") {
+                    println! ("[ERROR] Invalid audio extension on file {}, only OGG is supported. Ignoring file.", fixed_name);
+                }
             }
         }
 
