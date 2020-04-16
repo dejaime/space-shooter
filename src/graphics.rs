8a766c83f31fe6a6678204f490a7d58b27a08c8c
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-15 23:39:45 -0300

    Added load_spritesheet method

--- src/graphics.rs
+++ src/graphics.rs
@@ -17,8 +17,18 @@ fn load_texture(world: &World, image_file: &str) -> Handle<Texture> {
     loader.load(image_file, ImageFormat::default(), (), &texture_storage)
 }
 
-pub fn initialise_graphics(world: &mut World) {
+fn load_spritesheet(world: &World, image_file: &str, definition_file: &str) -> Handle<SpriteSheet> {
+    let loader = world.read_resource::<Loader>();
+    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+    let texture_handle = load_texture(world, image_file);
+    loader.load(
+        definition_file,
+        SpriteSheetFormat(texture_handle),
+        (),
+        &sprite_sheet_store,
+    )
+}
 
-fn load_sprite_sheets(world: &mut World) {
+pub fn initialise_graphics(world: &mut World) {
 
 }
