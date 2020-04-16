c1108a5f4010e514bb1ad8b6e838400e05a7f62c
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-16 11:47:13 -0300

    Addint sprite to player ship init

--- src/entity/player_ship.rs
+++ src/entity/player_ship.rs
@@ -1,24 +1,26 @@
 use amethyst::{
-    assets::Handle,
     core::{math::Vector3, transform::Transform},
     ecs::prelude::{Entity, World, WorldExt},
     prelude::*,
-    renderer::{SpriteRender, SpriteSheet},
+    renderer::{SpriteRender},
     window::ScreenDimensions,
 };
 
 use crate::component::ship_component::Ship;
+use crate::graphics::{get_spritesheet_handle};
 
 const SHIP_COLLISION_RADIUS: f32 = 32.0;
 const SHIP_SPAWN_Y_OFFSET: f32 = 64.0;
 
-pub fn spawn_player_ship(world: &mut World, sprite_sheet_handle: Handle<SpriteSheet>) -> Entity {
+pub fn spawn_player_ship(world: &mut World) -> Entity {
     //I need to use world.read_resource::<ScreenDimensions>() inside this short scope
     //  so I can use world.create_entity below (immutable (here) vs mutable borrow)
     let screen_dimensions = {
         let screen_dimensions_resource = world.read_resource::<ScreenDimensions>();
         (screen_dimensions_resource.width(), screen_dimensions_resource.height())
     };
+
+    let sprite_sheet_handle = get_spritesheet_handle(world, "player_1");
 
     let mut local_transform = Transform::default();
 
@@ -36,7 +38,7 @@ pub fn spawn_player_ship(world: &mut World, sprite_sheet_handle: Handle<SpriteSh
         })
         .with(local_transform)
         .with(SpriteRender {
-            sprite_sheet: sprite_sheet_handle.clone(),
+            sprite_sheet: sprite_sheet_handle,
             sprite_number: 0,
         })
         .build()
