e5d5a2e91df8b5680c337d5c14b66852051ed350
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-15 23:25:45 -0300

    Adding texture load function

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,12 +1,23 @@
 use amethyst::{
     assets::{AssetStorage, Handle, Loader},
-    core::{timing::Time, transform::Transform},
-    ecs::prelude::{Component, DenseVecStorage, Entity},
     prelude::*,
     renderer::{Camera, ImageFormat, SpriteRender, SpriteSheet, SpriteSheetFormat, Texture},
-    ui::{Anchor, TtfFormat, UiText, UiTransform},
 };
 
+use std::collections::HashMap;
+
+#[derive(Clone)]
+pub struct TextureResource {
+    //pub texture_map: HashMap<str, Texture>
+}
+
+fn load_texture(world: &World, image_file: &str) -> Handle<Texture> {
+    let loader = world.read_resource::<Loader>();
+    let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+    loader.load(image_file, ImageFormat::default(), (), &texture_storage)
+}
+
+pub fn initialise_graphics(world: &mut World) {
 
 fn load_sprite_sheets(world: &mut World) {
 
