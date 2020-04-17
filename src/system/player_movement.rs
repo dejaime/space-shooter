09e7b49968350093a2d63fd5061736b91b0fd866
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-16 23:58:47 -0300

    adding sprite render write access to movement system

--- src/system/player_movement.rs
+++ src/system/player_movement.rs
@@ -1,7 +1,10 @@
-use amethyst::core::{math::Vector2, timing::Time, Transform};
-use amethyst::derive::SystemDesc;
-use amethyst::ecs::{Join, Read, ReadStorage, System, SystemData, WriteStorage};
-use amethyst::input::{InputHandler, StringBindings};
+use amethyst::{
+    core::{math::Vector2, timing::Time, Transform},
+    derive::SystemDesc,
+    ecs::{Join, Read, ReadStorage, System, SystemData, WriteStorage},
+    input::{InputHandler, StringBindings},
+    renderer::SpriteRender,
+};
 
 #[derive(SystemDesc)]
 pub struct PlayerMovementSystem;
@@ -18,15 +21,18 @@ const X_MIN: f32 = -940.0;
 impl<'s> System<'s> for PlayerMovementSystem {
     type SystemData = (
         WriteStorage<'s, Transform>,
+        WriteStorage<'s, SpriteRender>,
         ReadStorage<'s, Player>,
         ReadStorage<'s, Ship>,
         Read<'s, InputHandler<StringBindings>>,
         Read<'s, Time>,
     );
 
-    fn run(&mut self, (mut transforms, players, ships, input, time): Self::SystemData) {
+    fn run(
+        &mut self,
+        (mut transforms, mut sprite_render, players, ships, input, time): Self::SystemData,
+    ) {
         for (player, ship, transform) in (&players, &ships, &mut transforms).join() {
-
             let (horizontal_movement, vertical_movement) = match player.seat {
                 PlayerSeat::P1 => (
                     input.axis_value("p1_horizontal").unwrap_or(0.0),
@@ -38,7 +44,9 @@ impl<'s> System<'s> for PlayerMovementSystem {
                 ),
             };
 
-            if horizontal_movement * horizontal_movement < 0.1 && vertical_movement * vertical_movement < 0.1 {
+            if horizontal_movement * horizontal_movement < 0.1
+                && vertical_movement * vertical_movement < 0.1
+            {
                 continue;
             }
 
@@ -47,8 +55,16 @@ impl<'s> System<'s> for PlayerMovementSystem {
                 * ship.speed
                 * time.delta_seconds();
 
-            transform.set_translation_x((transform.translation().x + direction.x).min(X_MAX).max(X_MIN));
-            transform.set_translation_y((transform.translation().y + direction.y).min(Y_MAX).max(Y_MIN));
+            transform.set_translation_x(
+                (transform.translation().x + direction.x)
+                    .min(X_MAX)
+                    .max(X_MIN),
+            );
+            transform.set_translation_y(
+                (transform.translation().y + direction.y)
+                    .min(Y_MAX)
+                    .max(Y_MIN),
+            );
         }
     }
 }
