8198b036d94b871612414a52e5e97fc763a939c4
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-17 16:17:58 -0300

    making state modules pub

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,12 +1,12 @@
-mod menu_state;
+pub mod menu_state;
 pub use self::menu_state::MenuState;
 
-mod space_state;
+pub mod space_state;
 pub use self::space_state::SpaceState;
 
-mod pause_state;
+pub mod pause_state;
 pub use self::pause_state::PauseState;
 
-mod loading_state;
+pub mod loading_state;
 pub use self::loading_state::LoadingState;
 
