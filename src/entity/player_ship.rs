ddb67f1a91457ac8896e91e4046fd936fb994b6e
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-16 22:47:30 -0300

    Setting player ship speed in a constant

--- src/entity/player_ship.rs
+++ src/entity/player_ship.rs
@@ -2,44 +2,52 @@ use amethyst::{
     core::{math::Vector3, transform::Transform},
     ecs::prelude::{Entity, World, WorldExt},
     prelude::*,
-    renderer::{SpriteRender},
+    renderer::SpriteRender,
     window::ScreenDimensions,
 };
 
-use crate::component::{ship_component::Ship, player_component::{Player, PlayerSeat}};
-use crate::graphics::{get_spritesheet_handle};
-
+use crate::component::{
+    player_component::{Player, PlayerSeat},
+    ship_component::Ship,
+};
+use crate::graphics::get_spritesheet_handle;
 
 const SHIP_COLLISION_RADIUS: f32 = 32.0;
-const SHIP_SPAWN_Y_OFFSET: f32 = 0.0;
+const SHIP_SPEED: f32 = 400.0;
 
 pub fn spawn_player_ship(world: &mut World, second_player: bool) -> Entity {
     //I need to use world.read_resource::<ScreenDimensions>() inside this short scope
     //  so I can use world.create_entity below (immutable (here) vs mutable borrow)
     let screen_dimensions = {
         let screen_dimensions_resource = world.read_resource::<ScreenDimensions>();
-        (screen_dimensions_resource.width(), screen_dimensions_resource.height())
+        (
+            screen_dimensions_resource.width(),
+            screen_dimensions_resource.height(),
+        )
     };
 
-    let (sprite_name, player_seat) = if second_player { ("player_2", PlayerSeat::P2) } else { ("player_1", PlayerSeat::P1) };
-    
+    let (sprite_name, player_seat) = if second_player {
+        ("player_2", PlayerSeat::P2)
+    } else {
+        ("player_1", PlayerSeat::P1)
+    };
 
     let sprite_sheet_handle = get_spritesheet_handle(world, sprite_name);
 
     let mut local_transform = Transform::default();
 
-    local_transform.set_translation(Vector3::new(
-        screen_dimensions.0 / 2.0 - SHIP_COLLISION_RADIUS,
-        SHIP_SPAWN_Y_OFFSET,
-        0.0,
-    ));
+    let spawn_y = if second_player {128.0} else {0.0};
+    local_transform.set_translation(Vector3::new(0.0, spawn_y, 0.0));
 
     world
         .create_entity()
-        .with(Player{seat: player_seat, ..Default::default()})
+        .with(Player {
+            seat: player_seat,
+            ..Default::default()
+        })
         .with(Ship {
             radius: 0.0,
-            speed: 200.0,
+            speed: SHIP_SPEED,
         })
         .with(local_transform)
         .with(SpriteRender {
