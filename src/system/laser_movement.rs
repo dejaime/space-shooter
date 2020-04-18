480b56ea55466b3f8ab19b4ae14b16df1e50aafe
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-17 21:43:48 -0300

    moving lasers

--- src/system/laser_movement.rs
+++ src/system/laser_movement.rs
@@ -1,7 +1,7 @@
 use amethyst::{
     core::{timing::Time, Transform},
     derive::SystemDesc,
-    ecs::{Entities, Join, Read, ReadStorage, System, SystemData, WriteStorage},
+    ecs::{Entities, Join, Read, System, SystemData, WriteStorage},
 };
 
 #[derive(SystemDesc)]
@@ -9,17 +9,37 @@ pub struct LaserMovementSystem;
 
 use crate::component::laser_component::Laser;
 
+const Y_MAX: f32 = 600.0;
+const Y_MIN: f32 = -600.0;
+const X_MAX: f32 = 1000.0;
+const X_MIN: f32 = -1000.0;
+
 impl<'s> System<'s> for LaserMovementSystem {
     type SystemData = (
         Entities<'s>,
         WriteStorage<'s, Transform>,
-        ReadStorage<'s, Laser>,
-        Read<'s, Time>
+        WriteStorage<'s, Laser>,
+        Read<'s, Time>,
     );
 
-    fn run(&mut self, (entities, mut transforms, lasers, time): Self::SystemData) {
-        for (laser_entity, transform, laser) in (&*entities, &mut transforms, &lasers).join() {
-            //TODO: Move Lasers
+    fn run(&mut self, (entities, mut transforms, mut lasers, time): Self::SystemData) {
+        for (laser_entity, transform, laser) in (&*entities, &mut transforms, &mut lasers).join() {
+            if transform.translation().x > X_MAX
+                || transform.translation().x < X_MIN
+                || transform.translation().y > Y_MAX
+                || transform.translation().y < Y_MIN
+            {
+                let _ = entities.delete(laser_entity);
+                continue;
+            }
+
+            let delta = time.delta_seconds();
+
+            //Apply movement and rotation before modifying it with accelerations and torques
+            transform.prepend_translation(laser.directional_speed * delta);
+            transform.rotate_2d(laser.rotational_radian_speed * delta);
+            laser.directional_speed += laser.directional_acceleration * delta;
+            laser.directional_acceleration += laser.directional_torque * delta;
         }
     }
 }
