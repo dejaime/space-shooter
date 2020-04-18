118cf5bda4d2fa8211d5588597c82492c5cb68cf
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-17 21:07:00 -0300

    adding laser and enemy_laser components to mod

--- src/component/mod.rs
+++ src/component/mod.rs
@@ -11,5 +11,11 @@ pub use self::ship_component::Ship;
 pub mod player_laser_component;
 pub use self::player_laser_component::PlayerLaser;
 
+pub mod enemy_laser_component;
+pub use self::enemy_laser_component::EnemyLaser;
+
+pub mod laser_component;
+pub use self::laser_component::Laser;
+
 pub mod prop_component;
 pub use self::prop_component::Prop;
