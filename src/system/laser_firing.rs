599823a6e8ce1a659ce784856e1624c55445192a
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-20 00:55:31 -0300

    laser firing system

--- src/system/laser_firing.rs
+++ src/system/laser_firing.rs
@@ -1,24 +1,44 @@
 use amethyst::{
-    core::{timing::Time, Transform},
+    core::{math::Vector3, timing::Time, Transform},
     derive::SystemDesc,
-    ecs::{Entities, Join, Read, ReadStorage, System, SystemData, WriteStorage},
+    ecs::{Entities, Join, LazyUpdate, Read, ReadExpect, ReadStorage, System, SystemData, WriteStorage},
 };
 
-use crate::component::weapon_type::*;
-use crate::component::weapon_component::Weapon;
+use crate::component::{laser_type::*, weapon_component::Weapon};
+use crate:: graphics::SpritesHolder;
+use crate::entity::laser::spawn_laser;
 
 #[derive(SystemDesc)]
 pub struct LaserFiringSystem;
 
 impl<'s> System<'s> for LaserFiringSystem {
     type SystemData = (
-        Entities<'s>,
         ReadStorage<'s, Transform>,
         WriteStorage<'s, Weapon>,
-        Read<'s, Time>
+        Read<'s, Time>,
+        ReadExpect<'s, LazyUpdate>,
+        ReadExpect<'s, SpritesHolder>,
+        Entities<'s>,
     );
 
-    fn run(&mut self, (_entities, _transforms, mut _weapons, _time): Self::SystemData) {
-        //TODO: FIRE!
+    fn run(&mut self, (transforms, mut weapons, time, lazy_update, sprite_sheet_holder, entities): Self::SystemData) {
+        for (transform, weapon) in (&transforms, &mut weapons).join() {
+            if weapon.next_shot_time <= 0.0 {
+                weapon.next_shot_time += weapon.cooldown;
+                //TODO: Test Weapon Type and create correct laser
+
+                let spawn_point =
+                    Vector3::new(transform.translation().x, transform.translation().y + 32.0, 0.01);
+                spawn_laser(
+                    &entities,
+                    &lazy_update,
+                    &sprite_sheet_holder,
+                    LaserType::Player(PlayerLaser::Simple),
+                    spawn_point,
+                );
+            } else {
+                weapon.next_shot_time -= time.delta_seconds();
+            }
+        }
     }
 }
