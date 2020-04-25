dd1dccc853c8ac2e18d8a44df63b8bfc44b30a08
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-25 19:20:35 -0300

    adding an offset to the follow target

--- src/component/clingy_component.rs
+++ src/component/clingy_component.rs
@@ -3,6 +3,7 @@ use amethyst::core::math::Vector3;
 #[derive(Component)]
 pub struct ClingTo {
     target: Entity,
+    offset: Vector3,
     max_distance: f32,
     speed: f32,
     perfect_follow: bool,
