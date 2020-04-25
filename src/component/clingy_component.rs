46ba4c1fd231f85bffea54442f62d7eb7c12c89a
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-25 20:33:32 -0300

    implementing component trait for clingy_component

--- src/component/clingy_component.rs
+++ src/component/clingy_component.rs
@@ -1,10 +1,15 @@
 use amethyst::core::math::Vector3;
 
-#[derive(Component)]
+use amethyst::ecs::{Component, Entity, VecStorage};
+
 pub struct ClingTo {
     target: Entity,
-    offset: Vector3,
+    offset: Vector3<f32>,
     max_distance: f32,
     speed: f32,
     perfect_follow: bool,
+}
+
+impl Component for ClingTo {
+    type Storage = VecStorage<Self>;
 }
