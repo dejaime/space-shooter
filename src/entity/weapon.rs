5151ebc62565a1ae6f4c5f4e4994f77e9f826d4b
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-04-26 01:03:47 -0300

    adding weapon entity

---
+++ src/entity/weapon.rs
@@ -0,0 +1,20 @@
+use amethyst::{
+    core::{
+        math::{Vector2, Vector3},
+        timing::Time,
+        transform::{Parent, Transform},
+    },
+    ecs::prelude::{Entity, World},
+    prelude::*,
+};
+
+use crate::component::weapon_component::Weapon;
+
+pub fn spawn_weapon_entity(world: &mut World, weapon_component: Weapon, owner: &mut Entity) -> Entity {
+    world
+        .create_entity()
+        .with(weapon_component)
+        .with(Transform::default())
+        .with(Parent::new(*owner))
+        .build()
+}
