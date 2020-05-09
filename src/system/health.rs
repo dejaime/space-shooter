a112d29e8df3d710f00d7ef03b4740d004a1b3cf
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-05-09 16:17:56 -0300

    destroying ships when out of health

--- src/system/health.rs
+++ src/system/health.rs
@@ -7,24 +7,35 @@ use amethyst::{
 use crate::component::{
     enemy_component::Enemy,
     player_component::{Player, PlayerSeat},
+    health_component::Health,
 };
 
-pub struct ClingSystem;
+pub struct HealthSystem;
 
-impl<'s> System<'s> for ClingSystem {
+impl<'s> System<'s> for HealthSystem {
     type SystemData = (
         Entities<'s>,
         WriteStorage<'s, Health>,
         ReadStorage<'s, Player>,
         ReadStorage<'s, Enemy>,
+        Read<'s, Time>
     );
 
-    fn run(&mut self, (entities, mut healths, players, enemies): Self::SystemData) {
-        for (health, players) in (&healths, &players).join() {
+    fn run(&mut self, (entities, mut healths, players, enemies, time): Self::SystemData) {
+        for (player_entity, health, player) in (&*entities, &mut healths, &players).join() {
+            health.time_since_last_hit += time.delta_seconds();
+            if health.health <= 0.0 {
+                //TODO: deal with game over
+                let _ = entities.delete(player_entity);
+            }
             continue;
         }
 
-        for (health, enemies) in (&healths, &enemies).join() {
+        for (enemy_entity, health, enemy) in (&*entities, &mut healths, &enemies).join() {
+            health.time_since_last_hit += time.delta_seconds();
+            if health.health <= 0.0 {
+                let _ = entities.delete(enemy_entity);
+            }
             continue;
         }
     }
