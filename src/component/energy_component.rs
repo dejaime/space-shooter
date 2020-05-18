66389a0964e967a6d211efb9a398a43693f66e19
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-05-17 22:42:30 -0300

    Creating energy component

---
+++ src/component/energy_component.rs
@@ -0,0 +1,14 @@
+use amethyst::ecs::prelude::{Component, DenseVecStorage};
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Energy {
+    pub energy: f32,
+    pub max_energy: f32,
+    pub energy_recovery_per_second: f32,
+    pub recover_cooldown: f32,
+    pub last_activation: f32,
+}
+
+impl Component for Energy {
+    type Storage = DenseVecStorage<Self>;
+}
