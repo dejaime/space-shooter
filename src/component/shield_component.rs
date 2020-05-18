b76a822740f890f2c7dd14740f2ec8f9b53d52b8
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-05-18 01:13:58 -0300

    Adding shield component

---
+++ src/component/shield_component.rs
@@ -0,0 +1,19 @@
+use amethyst::{
+    ecs::prelude::{Component, VecStorage},
+};
+
+use crate::component::{player_component::PlayerSeat};
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct Shield {
+    pub owner_seat: PlayerSeat,
+    pub hit_points: f32,
+    pub cooldown: f32,
+    pub time_since_last_hit: f32,
+    pub time_since_last_break: f32,
+    pub hit_point_recovery_per_second: f32,
+}
+
+impl Component for Shield {
+    type Storage = VecStorage<Self>;
+}
