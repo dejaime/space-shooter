9f4d5925fd00986e73c8e41c10fcf8573988bcf3
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-05-18 02:49:14 -0300

    adding a sprite to shield entity

--- src/entity/shield.rs
+++ src/entity/shield.rs
@@ -1,18 +1,38 @@
 use amethyst::{
-    core::{
-        transform::{Parent, Transform},
-    },
+    core::transform::{Parent, Transform},
     ecs::prelude::{Entity, World},
     prelude::*,
+    renderer::SpriteRender,
 };
+
+use crate::graphics::SpritesHolder;
 
 use crate::component::shield_component::Shield;
 
-pub fn spawn_shield_entity(world: &mut World, shield_component: Shield, owner: &mut Entity) -> Entity {
+pub fn spawn_shield_entity(
+    world: &mut World,
+    shield_component: Shield,
+    owner: &mut Entity,
+) -> Entity {
+    let sprite_sheet;
+    {
+        let sprite_sheet_holder = &*(world.try_fetch::<SpritesHolder>().unwrap());
+
+        sprite_sheet = sprite_sheet_holder
+            .sprite_map
+            .get("explosion_1")
+            .unwrap()
+            .clone();
+    }
+
     world
         .create_entity()
         .with(shield_component)
         .with(Transform::default())
+        .with(SpriteRender {
+            sprite_sheet,
+            sprite_number: 8,
+        })
         .with(Parent::new(*owner))
         .build()
 }
