9aa86f7f13dabbf86a71b96ac9ce2834175a874b
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-05-17 22:59:13 -0300

    renaming time since activation variable to be an incremental timer

--- src/component/energy_component.rs
+++ src/component/energy_component.rs
@@ -6,7 +6,7 @@ pub struct Energy {
     pub max_energy: f32,
     pub energy_recovery_per_second: f32,
     pub recover_cooldown: f32,
-    pub last_activation: f32,
+    pub time_since_last_activation: f32,
 }
 
 impl Component for Energy {
