556c6d88f9c3f025f00abe61b4c4f610b77f5796
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-09-17 00:10:06 -0300

    Adding space props to the menu

--- src/state/menu_state.rs
+++ src/state/menu_state.rs
@@ -16,17 +16,29 @@ use crate::state::PropCounter;
 use crate::fonts::get_font;
 
 pub struct MenuState {
+    pub spawn_prop_timer: Option<f32>,
+    pub rng: ThreadRng,
 }
 
 impl Default for MenuState {
     fn default() -> Self {
         MenuState {    
+            spawn_prop_timer: Some(1.0),
+            rng: thread_rng(),
         }
     }
 }
 
 impl SimpleState for MenuState {
-    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {}
+    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+
+        self.rng = thread_rng();
+
+        prop_warm_up(data.world, &mut self.rng);
+
+        data.world.insert(PropCounter {
+            ..Default::default()
+        });
 
         let one_player_button_uitransform = UiTransform::new(
             String::from("1p_btn"),
@@ -75,6 +87,22 @@ impl SimpleState for MenuState {
 
     fn update(&mut self, data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
 
+        let delta_time = data.world.fetch::<Time>().delta_seconds();
+
+        if let Some(mut timer) = self.spawn_prop_timer.take() {
+            timer -= delta_time;
+            if timer <= 0.0 {
+                {
+                    let mut prop_counter = data.world.fetch_mut::<PropCounter>();
+                    prop_counter.active_props_count += 1;
+                    prop_counter.total_spawned_props += 1;
+                }
+                spawn_prop(data.world, &mut self.rng);
+                self.spawn_prop_timer.replace(self.rng.gen_range(0.01, 0.3));
+            } else {
+                self.spawn_prop_timer.replace(timer);
+            }
+        }
 
         Trans::None
         // Trans::Push(Box::new(SpaceState {
