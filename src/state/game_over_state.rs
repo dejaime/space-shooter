73cd6511e54118b36161f96ebb21feada266fc78
Dejaime Antonio de Oliveira Neto <dejneto@gmail.com>
2020-09-17 04:13:12 -0300

    Adding mock Game Over state

--- src/state/game_over_state.rs
+++ src/state/game_over_state.rs
@@ -4,4 +4,11 @@ pub struct GameOverState;
 
 impl SimpleState for GameOverState {
     fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {}
+
+    fn update(&mut self, _data: &mut StateData<'_, GameData<'_, '_>>) -> SimpleTrans {
+        use crate::state::LoadingState;
+        return Trans::Replace(Box::new(LoadingState {
+            ..Default::default()
+        }));
+    }
 }
